192bd96edf9d29c3da4ed91a922fb3dc8c5b4e69
Jose Landete <joseantoniolandete@gmail.com>
2018-09-06 09:51:07 +0200

    prueba Lombok

--- src/main/java/com/example/demoJPA/entity/Coche.java
+++ src/main/java/com/example/demoJPA/entity/Coche.java
@@ -2,15 +2,26 @@ package com.example.demoJPA.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Table (name="coche")
 public class Coche {
 
--- src/main/java/com/example/demoJPA/entity/Persona.java
+++ src/main/java/com/example/demoJPA/entity/Persona.java
@@ -3,7 +3,6 @@ package com.example.demoJPA.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,21 +12,22 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table (name="persona")
+
+@Table(name = "persona")
 public class Persona {
 
 	@Id
-	@Column(name="Dni")
+	@Column(name = "Dni")
 	private String dni;
-	
-	@Column(name="Nombre",nullable=true)
+
+	@Column(name = "Nombre", nullable = true)
 	private String nombre;
-	
-	@Column(name="Fecha_nac",nullable=true)
+
+	@Column(name = "Fecha_nac", nullable = true)
 	private String fechaNac;
-	
-//	@OneToMany(mappedBy="persona")
-//	private List<Coche>coches= new ArrayList<Coche>();
+
+	@OneToMany(mappedBy = "persona")
+	private List<Coche> coches = new ArrayList<Coche>();
 
 	public String getDni() {
 		return dni;
@@ -61,14 +61,12 @@ public class Persona {
 //		this.coches = coches;
 //	}
 
-
-
 	public Persona(String dni, String nombre, String fechaNac, List<Coche> coches) {
 		super();
 		this.dni = dni;
 		this.nombre = nombre;
 		this.fechaNac = fechaNac;
-		//this.coches = coches;
+		// this.coches = coches;
 	}
 
 	public Persona() {
@@ -84,10 +82,5 @@ public class Persona {
 //	public String toString() {
 //		return "Persona [dni=" + dni + ", nombre=" + nombre + ", fechaNac=" + fechaNac + ", coches=" + coches + "]";
 //	}
-	
-
-	
-	
-	
 
 }
